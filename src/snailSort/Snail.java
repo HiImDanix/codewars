2d3a1b35be333b6b4abc6359fcd2f04c95c2683f
Daniels Kanepe <danixkanepe@gmail.com>
2022-03-10 16:49:47 +0100

    Shortened & made internal methods private

--- src/snailSort/Snail.java
+++ src/snailSort/Snail.java
@@ -1,53 +1,36 @@
 package snailSort;
 
-import java.util.Arrays;
-
 public class Snail {
 	
-	public static void main(String[] args) {
-        int[][] array
-        = {{}};
-		System.out.println(Arrays.toString(Snail.snail(array)));
-	}
 	
-	
-	enum Move {
+	private enum Move {
 		UP,
 		DOWN,
 		LEFT,
 		RIGHT,
 		FINISHED
 	}
-	
-	private Snail() {}
 
 	public static int[] snail(int[][] array) {
-		int n = array.length;
-
-		if (array[0].length == 0) {
-			return new int[0];
-		}
+		int n = array[0].length;
 		
-		int xRightBound = n; // The current bound for positive X (right direction)
-		int yBotBound = n; // the current bound for positive Y (down direction)
+		int xRightBound = n-1; // The current bound for positive X (right direction)
+		int yBotBound = n-1; // the current bound for positive Y (down direction)
 		int xLeftBound = 0;
 		int yUpBound = 0;
 		
 		int[] newArray = new int[n*n];
-		if (n > 0) {
+		int currX = 0;
+		int currY = 0;
+		// Because we are already at 0, 0
+		if (n > 0) { 
 			newArray[0] = array[0][0];
 		}
 		
-		int currX = 0;
-		int currY = 0;
 		Move lastMove = null;
 		Move nextMove = null;
 		int i = 1;
-		while (true) {
-			nextMove = nextMove(currX, currY, xRightBound, yBotBound, xLeftBound, yUpBound, lastMove);
-			if (nextMove == Move.FINISHED) {
-				break;
-			}
+		while ((nextMove = nextMove(currX, currY, xRightBound, yBotBound, xLeftBound, yUpBound, lastMove)) != Move.FINISHED) {
 			
 			if (nextMove.equals(Move.RIGHT)) {
 				currX++;
@@ -70,7 +53,7 @@ public class Snail {
 					yBotBound--;
 				}
 			}
-			System.out.println("x right bound: " + (xRightBound - 1));
+			System.out.println("x right bound: " + (xRightBound));
 			System.out.println("currx: " + currX + " curry: " + currY + " move: " + nextMove + " value: " + array[currY][currX]);
 			
 			newArray[i++] = array[currY][currX];
@@ -82,23 +65,23 @@ public class Snail {
 		
 	} 
 	
-	public static boolean canMoveRight(int currX, int xRightBound) {
-		return (currX+1 <= xRightBound - 1);
+	private static boolean canMoveRight(int currX, int xRightBound) {
+		return (currX+1 <= xRightBound);
 	}
 	
-	public static boolean canMoveDown(int currY, int yBotBound) {
-		return (currY+1 <= yBotBound - 1);
+	private static boolean canMoveDown(int currY, int yBotBound) {
+		return (currY+1 <= yBotBound);
 	}
 	
-	public static boolean canMoveLeft(int currX, int xLeftBound) {
+	private static boolean canMoveLeft(int currX, int xLeftBound) {
 		return (currX-1 >= xLeftBound);
 	}
 	
-	public static boolean canMoveUp(int currY, int yUpBound) {
+	private static boolean canMoveUp(int currY, int yUpBound) {
 		return (currY-1 >= yUpBound);
 	}
     
-	public static Move nextMove(int currX, int currY, int xRightBound, int yBotBound, int xLeftBound,
+	private static Move nextMove(int currX, int currY, int xRightBound, int yBotBound, int xLeftBound,
 			int yUpBound, Move lastMove) {
 		// if array size is 0, there is no next move
 		if (xRightBound == 0 && yBotBound == 0) {
